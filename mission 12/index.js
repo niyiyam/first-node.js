a939efbe578a92d86271aa02a822cf2a3a527a6b
niyiyam <hanjun2430@daum.net>
2022-08-04 23:39:48 +0900

    mission 12

---
+++ mission 12/index.js
@@ -0,0 +1,3 @@
+let now = new Date()
+let dateString = `${now.getFullYear()}년 ${now.getMonth()+1}월 ${now.getDate()}일 ${now.getHours()}시 ${now.getMinutes()}분 ${now.getSeconds()}초`
+console.log(dateString)
