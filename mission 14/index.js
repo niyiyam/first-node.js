648f3e4071aa33940c776654a62ea0925eb21d2b
niyiyam <hanjun2430@daum.net>
2022-08-05 03:23:21 +0900

    mission 14

---
+++ mission 14/index.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+let func = () =>{
+    const [number, setnumber] = useState(0);
+    let Increase = () => {
+        setnumber(number + 1);
+    }
+    let Decrease = () => {
+        setnumber(number -1);
+    }
+}
+
+function App() {
+    return (
+        <div>
+            <h1>{number}</h1>
+            <button onClick={Increase}>+1</button>
+            <button onClick={Decrease}>-1</button>
+        </div>
+    );
+  }
+  
+export default App
